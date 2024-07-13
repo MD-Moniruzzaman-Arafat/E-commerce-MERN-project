e385e25bbd5df0d0d52b1e4c2d4297ee21d800a8
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2024-07-13 23:12:25 +0600

    api testing using post man and middleware and type of middleware

--- server/src/server.js
+++ server/src/server.js
@@ -4,19 +4,36 @@ const app = express();
 const port = 3001;
 
 app.use(morgan("dev"));
+app.use(express.json());
+// app.use(express.urlencoded({ extends: true }));
+
+const isLogin = (req, res, next) => {
+  const isLogin = true;
+  if (isLogin) {
+    req.body.id = 101;
+    next();
+  } else {
+    res.status(401).json({ message: "please log in" });
+  }
+  console.log("user is login");
+};
+
+// app.use(isLogin);
 
 app.get("/", (req, res) => {
   res.send("welcome to the express server");
 });
 
-app.get("/products", (req, res) => {
-  res.send("product is required");
-});
-
-app.get("/res", (req, res) => {
+app.get("/res", isLogin, (req, res) => {
+  console.log(req.body.id);
   res.status(200).send({
     message: "server is ok",
   });
+});
+
+// client error handling middleware
+app.use((req, res, next) => {
+  res.status(404).json({ message: "not found" });
 });
 
 app.listen(port, () => {
